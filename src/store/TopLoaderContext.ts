e19be27b88a68f90c06df20fcaca590000c04d33
Alessio Frittoli <35973186+alessiofrittoli@users.noreply.github.com>
2025-08-06 13:28:35 +0200

    docs: add TopLoader Context API Reference (#10)

--- src/store/TopLoaderContext.ts
+++ src/store/TopLoaderContext.ts
@@ -4,6 +4,9 @@ import type { UseTopLoaderApi } from '@/hooks'
 
 export type TopLoaderCtx = UseTopLoaderApi
 
-
+/**
+ * TopLoader React Context API.
+ * 
+ */
 export const TopLoaderContext	= createContext<TopLoaderCtx | undefined>( undefined )
 TopLoaderContext.displayName	= 'TopLoaderContext'
